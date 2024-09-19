9bebd019f7d11c1937e03d701ff99711cf0d8cd3
Mohammad Adnan <mohammadadnan6460@gmail,com>
2024-09-20 01:49:19 +0530

    mongo

--- models/user.js
+++ models/user.js
@@ -3,9 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 // mongoose.connect(process.env.MONGO_URL)
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected!'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URL);
 
 const userSchema = mongoose.Schema({
     name: String,
