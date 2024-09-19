818d62322b994d0e3f8645a4faf727ff04af4e2e
Mohammad Adnan <mohammadadnan6460@gmail,com>
2024-09-20 01:32:54 +0530

    mongo

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ##Credentials
 MONGO_URL=mongodb://localhost:27017/miniproj
 PORT=3003
+
+quillnotes
+qIY6irXpqB5lpKp1
+
+mongodb+srv://<username>:<password>@cluster-url/<dbname>?retryWrites=true&w=majority
--- models/user.js
+++ models/user.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URL)
+// mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected!'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 const userSchema = mongoose.Schema({
     name: String,
